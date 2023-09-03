e270f4b7c83b0fa655d44075d33b905c445e7575
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2023-09-03 20:13:06 +0530

    Time: 51 ms (68.14%) | Memory: 23.8 MB (69.03%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isPalindrome(int l,int r, string s){
+        while(l<r){
+            if(s[l]!=s[r])return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int allowed=0;
+        int low = 0, high=s.size()-1;
+        while(high>low){
+            if(s[high]==s[low]){
+                high--;
+                low++;
+            }
+            else{
+                if(isPalindrome(low,high-1,s)){
+                    allowed++;
+                    high-=2;
+                    low++;
+                }
+                else if(isPalindrome(low+1,high,s)){
+                    allowed++;
+                    low+=2;
+                    high--;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(allowed<=1)return true;
+        return false;
+    }
+};
