140c83434846292d8e0950c440fb30c49ea4ee50
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2023-09-03 09:33:26 +0530

    Time: 104 ms (64.19%) | Memory: 69.5 MB (64.67%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(begin(nums),end(nums));
-        for(int i=0;i<nums.size()-1;i++){
-            if(binary_search(begin(nums)+i+1,end(nums),nums[i]))
-            {
+        unordered_map<int,int>ans;
+        for(int i=0;i<nums.size();i++){
+            ans[nums[i]]++;
+            if(ans[nums[i]]>1){
                 return true;
             }
         }
