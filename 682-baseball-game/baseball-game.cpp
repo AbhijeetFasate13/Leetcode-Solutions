8081543d52f10ebf521e0638292b87fbfa7efdd9
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2023-09-03 20:01:46 +0530

    Time: 3 ms (81.10%) | Memory: 8.5 MB (5.89%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -3,36 +3,28 @@ public:
     int calPoints(vector<string>& operations) {
         stack<int>check;
         int sum = 0, n = operations.size();
+        int a,b;
         for(int i=0;i<n;i++){
-            int flag=0;
-            if(operations[i][0]=='-'){
-                operations[i]=operations[i].substr(1);
-                flag=1;
-            }
-            if(operations[i][0]>='0' and operations[i][0]<='9'){
-                int k = stoi(operations[i]);
-                if(flag){
-                    k*=-1;
-                }
-                check.push(k);
-                sum+=k;
-            }
-            else if(operations[i]=="+"){
-                int temp = check.top();
+            if(operations[i]=="+"){
+                a=check.top();
                 check.pop();
-                int temp1 = check.top();
-                check.push(temp);
-                check.push(temp+temp1);
-                sum+=temp+temp1;
+                b=check.top();
+                check.push(a);
+                check.push(a+b);
+            }
+            else if(operations[i]=="D"){
+                check.push(2*check.top());
             }
             else if(operations[i]=="C"){
-                sum-=check.top();
                 check.pop();
             }
             else{
-                sum+=2*check.top();
-                check.push(2*check.top());
+                check.push(stoi(operations[i]));
             }
+        }
+        while(!check.empty()){
+            sum+=check.top();
+            check.pop();
         }
         return sum;
     }
