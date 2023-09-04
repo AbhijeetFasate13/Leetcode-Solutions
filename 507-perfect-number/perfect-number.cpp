2ae0822dbb356a84ed01f6332d61bee5a155194d
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2023-09-04 22:45:25 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (44.91%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<num;i++){
+        if(num==1)return false;
+        int sum=1;
+        for(int i=2;i*i<num;i++){
             if(num%i==0){
-                sum+=i;
+                if(num/i==i){
+                    sum+=i;
+                }
+                else{
+                    sum+=num/i + i;
+                }
             }
         }
         return sum==num;
