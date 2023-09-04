ea108e1aef9425aed052da341d6bc564f0a94b44
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2023-09-04 23:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (33.65%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -5,15 +5,10 @@ public:
         return false;
     }
     int countPrimeSetBits(int left, int right) {
-        vector<int>dp(right+1);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=right;i++){
-            dp[i]=dp[i/2]+i%2;
-        }
         int count=0;
         for(int i=left;i<=right;i++){
-            if(isprime(dp[i]))count++;
+            int k = __builtin_popcount(i);
+            if(isprime(k))count++;
         }
         return count;
     }
