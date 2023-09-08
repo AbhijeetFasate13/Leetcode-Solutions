edec388edd7bdd61fb3a6aa85c03d5331ca7a181
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2023-09-08 19:41:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (61.88%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    void permutations(vector<vector<int>>&ans,vector<int>&nums,int i){
+        if(i==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=i;j<nums.size();j++){
+                swap(nums[i],nums[j]);
+                permutations(ans,nums,i+1); 
+                swap(nums[i],nums[j]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        permutations(ans,nums,0);
+        return ans;
+    }
+};
