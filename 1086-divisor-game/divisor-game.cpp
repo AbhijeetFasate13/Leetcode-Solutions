b80b89820bcdd5b889ae69bfc43254872161ae9b
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2024-10-28 12:05:17 +0530

    Time: 2 ms (16.11%) | Memory: 7.7 MB (9.20%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        return !(n&1);
+        vector<int> dp(n + 1);
+        for (int i = 2; i <= n; i += 1) {
+            int ans = 0;
+            for (int j = 1; j * j <= i; j += 1) {
+                if (i % j == 0 and !dp[i - j]) {
+                    ans = 1;
+                    break;
+                }
+            }
+            dp[i] = ans;
+        }
+        return dp[n];
     }
 };
