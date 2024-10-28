970b7779cdb119920835a4909fb86d54565af959
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2024-10-28 13:33:23 +0530

    Time: 27 ms (97.09%) | Memory: 11.2 MB (74.35%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int numSquares(int n) {
         vector<int> dp(n + 1);
-        dp[0] = dp[1] = 1;
+        dp[0] = 0;
+        dp[1] = 1;
         for (int i = 2; i <= n; i++) {
             int minReq = INT_MAX;
             for (int j = 1; j * j <= i; j += 1) {
-                if (j * j == i) {
-                    minReq = 1;
-                    break;
-                }
                 minReq = min(minReq, 1 + dp[i - j * j]);
             }
             dp[i] = minReq;
