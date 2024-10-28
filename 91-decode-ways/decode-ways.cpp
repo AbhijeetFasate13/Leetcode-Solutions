a49363f3e2e6b3da296410d450bc0e3d73dd4867
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2024-10-28 13:12:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (47.08%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -6,7 +6,7 @@ public:
             return 0;
         vector<int> dp(n + 1);
         dp[0] = 1;
-        dp[1] = s[1] == '0' ? 0 : 1;
+        dp[1] = s[0] == '0' ? 0 : 1;
         for (int i = 2; i <= n; i++) {
             if (s[i - 1] != '0') {
                 dp[i] += dp[i - 1];
