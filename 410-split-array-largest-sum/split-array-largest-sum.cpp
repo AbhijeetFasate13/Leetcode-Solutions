0155e10ee9c9c7b9e12bd0948b401e9111b198df
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2024-11-03 12:39:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (88.81%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -3,28 +3,28 @@ public:
     int splitArray(vector<int>& nums, int k) {
         int n = (int)nums.size();
         auto fn = [&](int mid) {
-            int count = 1;
-            int sum = 0;
+            int groups = 0;
+            int temp = mid;
             for (int i = 0; i < n; i++) {
-                if (sum + nums[i] <= mid) {
-                    sum += nums[i];
+                if (temp - nums[i] >= 0) {
+                    temp -= nums[i];
                 } else {
-                    count++;
-                    sum = nums[i];
+                    groups++;
+                    temp = mid - nums[i];
                 }
             }
-            return count <= k;
+            return groups >= k;
         };
-        int l = *max_element(nums.begin(), nums.end()),
-            r = accumulate(nums.begin(), nums.end(), 0);
-        while (l < r) {
-            int mid = l + (r - l) / 2;
+        int l = *max_element(nums.begin(), nums.end()) - 1;
+        int r = accumulate(nums.begin(), nums.end(), 0) + 1;
+        while (l + 1 < r) {
+            int mid = l + (r - l + 1) / 2;
             if (fn(mid)) {
-                r = mid;
+                l = mid;
             } else {
-                l = mid + 1;
+                r = mid;
             }
         }
-        return l;
+        return r;
     }
 };
