15dacf7986bc63a6bdaec4e35e091fda4b725513
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-01-25 19:20:55 +0530

    Time: 11 ms (52.62%) | Memory: 31.8 MB (98.63%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -3,7 +3,7 @@ public:
     int arrayPairSum(vector<int>& nums) {
         sort(begin(nums), end(nums));
         int sum = 0;
-        for(int i = 0; i < (int)nums.size(); i+=2){
+        for(int i = 0; i < (int)nums.size() - 1; i+=2){
             sum += nums[i];
         }
         return sum;
