d607de8b91d255ce56b5597f54545c6f073da28d
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-01-26 22:07:08 +0530

    Time: 2 ms (27.20%) | Memory: 9.4 MB (28.35%) - LeetSync

---
+++ 851-goat-latin/goat-latin.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    bool isVowel(char c) {
+        if (c >= 'A' and c <= 'Z') {
+            c -= 'A';
+            c += 'a';
+        }
+        vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+        for (int i = 0; i < 5; i++) {
+            if (c == vowels[i])
+                return true;
+        }
+        return false;
+    }
+
+public:
+    string toGoatLatin(string sentence) {
+        stringstream ss(sentence);
+        vector<string> words;
+        string word;
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        int n = (int)words.size();
+        for (int i = 0; i < n; i++) {
+            if (!isVowel(words[i][0])) {
+                char temp = words[i][0];
+                for (int j = 0; j < (int)words[i].size() - 1; j++) {
+                    words[i][j] = words[i][j + 1];
+                }
+                words[i].back() = temp;
+            }
+            words[i] += "ma";
+            for (int j = 0; j < i + 1; j++) {
+                words[i].push_back('a');
+            }
+        }
+        string ans;
+        for (int i = 0; i < n; i++) {
+            ans += words[i];
+            ans += " ";
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
