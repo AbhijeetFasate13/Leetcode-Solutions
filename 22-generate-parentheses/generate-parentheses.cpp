8a45b9bd2a374d982b521f6a381d3db298b3a29a
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-06-22 10:37:30 +0530

    Time: 5 ms (20.21%) | Memory: 15.8 MB (36.24%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,22 +1,20 @@
 class Solution {
-    void rec(int idx, int n, int sum, string curr, vector<string>& allSets) {
-        if (idx == 2*n) {
-            if (sum == 0) {
-                allSets.push_back(curr);
-            }
+    void rec(int open, int close, string curr, vector<string>& allSets) {
+        if (open == 0 and close == 0) {
+            allSets.push_back(curr);
             return;
         }
-        rec(idx + 1, n, sum + 1, curr + "(", allSets);
-        if (sum != 0) {
-            rec(idx + 1, n, sum - 1, curr + ")", allSets);
+        if (open > 0)
+            rec(open - 1, close, curr + "(", allSets);
+        if (close > open) {
+            rec(open, close - 1, curr + ")", allSets);
         }
     }
 
 public:
     vector<string> generateParenthesis(int n) {
-        string curr;
         vector<string> allSets;
-        rec(0, n, 0, curr, allSets);
+        rec(n, n, "", allSets);
         return allSets;
     }
 };
