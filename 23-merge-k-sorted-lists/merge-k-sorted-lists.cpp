89de40b4e05a4b485bedb671c855e1d74ff126ea
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-07-03 10:24:18 +0530

    Time: 3 ms (64.37%) | Memory: 19.4 MB (10.15%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -9,33 +9,39 @@
  * };
  */
 class Solution {
-    class Compare {
-    public:
-        bool operator()(ListNode* a, ListNode* b) { return a->val > b->val; }
-    };
-
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
-        for (const auto& i : lists) {
-            if(i)
-                pq.push(i);
-        }
+    ListNode* mergeTwo(ListNode* a, ListNode* b) {
         ListNode* dummy = new ListNode();
         ListNode* temp = dummy;
-        ListNode* peek;
-        while (!pq.empty()) {
-            peek = pq.top();
-            pq.pop();
-            temp->next = peek;
-            temp = temp->next;
-            if (peek and peek->next) {
-                peek = peek->next;
-                pq.push(peek);
+        while (a && b) {
+            if (a->val < b->val) {
+                temp->next = a;
+                a = a->next;
+            } else {
+                temp->next = b;
+                b = b->next;
             }
+            temp = temp->next;
         }
+        temp->next = a ? a : b;
         temp = dummy->next;
         delete dummy;
         return temp;
+    }
+
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        deque<ListNode*> merged;
+        for (const auto& i : lists) {
+            if (i)
+                merged.push_back(i);
+        }
+        if (merged.empty())
+            return nullptr;
+        while (merged.size() > 1) {
+            merged.push_back(mergeTwo(merged[0], merged[1]));
+            merged.pop_front();
+            merged.pop_front();
+        }
+        return merged[0];
     }
 };
