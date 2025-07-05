941ff734b7b38b2c641f0c4e9a974ddfcf8101ab
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-07-05 14:19:27 +0530

    Time: 1 ms (80.26%) | Memory: 14.9 MB (21.45%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = (int)nums.size();
-        vector<int> indices(n);
-        iota(indices.begin(), indices.end(), 0);
-        sort(indices.begin(), indices.end(),
-             [&](const int& a, const int& b) { return nums[a] < nums[b]; });
-        int s = 0, e = n - 1, sum;
-        while (s < e) {
-            sum = nums[indices[s]] + nums[indices[e]];
-            if (sum == target) {
-                return {indices[s], indices[e]};
-            } else if (sum < target) {
-                s++;
-            } else {
-                e--;
+        unordered_map<int, int> numsToIdx;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int rem = target - nums[i];
+            if (numsToIdx.find(rem) != numsToIdx.end()) {
+                return {numsToIdx[rem], i};
             }
+            numsToIdx[nums[i]] = i;
         }
-        return {};
+        return {-1, -1};
     }
 };
