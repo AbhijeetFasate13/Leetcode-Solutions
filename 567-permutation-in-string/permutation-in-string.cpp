09147f2b933a463469d206c90c18c94e958f4398
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-07-18 21:14:34 +0530

    Time: 3 ms (69.67%) | Memory: 10.1 MB (34.69%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,14 +1,31 @@
 class Solution {
+    bool isEqual(const vector<int>& a, const vector<int>& b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i])
+                return false;
+        }
+        return true;
+    }
+
 public:
     bool checkInclusion(string s1, string s2) {
         if (s1.length() > s2.length())
             return false;
         int m = s1.length(), n = s2.length();
-        sort(s1.begin(), s1.end());
-        for (int i = 0; i <= n - m; i++) {
-            string temp = s2.substr(i, m);
-            sort(begin(temp), end(temp));
-            if (s1 == temp) {
+        vector<int> freq1(26), freq2(26);
+        for (int i = 0; i < m; i++) {
+            freq1[s1[i] - 'a']++;
+        }
+        for (int i = 0; i < m; i++) {
+            freq2[s2[i] - 'a']++;
+        }
+        if (isEqual(freq1, freq2)) {
+            return true;
+        }
+        for (int i = m; i < n; i++) {
+            freq2[s2[i] - 'a']++;
+            freq2[s2[i - m] - 'a']--;
+            if (isEqual(freq1, freq2)) {
                 return true;
             }
         }
