13ad8565fa4062f11daf6dfc5c8f66e79d8bed47
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-11-30 00:14:35 +0530

    Time: 2 ms (25.06%) | Memory: 8.7 MB (70.18%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,21 +1,29 @@
 class Solution {
+    vector<int> dp;
+    bool belongsTo(int tens, int units, int l, int r) {
+        int num = tens * 10 + units;
+        return num >= l and num <= r;
+    }
+    int rec(int idx, const string& s) {
+        if (idx >= s.size()) {
+            return 1;
+        }
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+        int curr = s[idx] - '0';
+        int next = idx != s.size() - 1 ? s[idx + 1] - '0' : -1;
+        int ways = 0;
+        ways += (curr != 0) ? rec(idx + 1, s) : 0;
+        if (next != -1) {
+            ways += belongsTo(curr, next, 10, 26) ? rec(idx + 2, s) : 0;
+        }
+        return dp[idx] = ways;
+    }
+
 public:
     int numDecodings(string s) {
-        int n = (int)s.size();
-        if (s[0] == '0')
-            return 0;
-        vector<int> dp(n + 1);
-        dp[0] = 1;
-        dp[1] = s[0] == '0' ? 0 : 1;
-        for (int i = 2; i <= n; i++) {
-            if (s[i - 1] != '0') {
-                dp[i] += dp[i - 1];
-            }
-            if (s.substr(i - 2, 2) >= "10" and s.substr(i - 2, 2) <= "26") {
-                dp[i] += dp[i - 2];
-                // cout << s.substr(i - 2, 2) << endl;
-            }
-        }
-        return dp[n];
+        dp.resize(s.size() + 1, -1);
+        return rec(0, s);
     }
 };
