e330caa08e88123702ecd76cbc1f48b0b2a3591d
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-11-29 23:05:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.58%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,15 @@
 class Solution {
+    int dp[46];
+
 public:
+    Solution() { memset(dp, -1, sizeof(dp)); }
     int climbStairs(int n) {
-        vector<int> dp(n + 1);
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        if (n <= 1) {
+            return 1;
         }
-        return dp[n];
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
