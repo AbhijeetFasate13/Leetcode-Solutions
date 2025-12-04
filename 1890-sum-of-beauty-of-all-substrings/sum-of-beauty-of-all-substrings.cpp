fb74633c61ae9364dc15068341f71c77da3e5a89
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-12-04 21:40:51 +0530

    Time: 63 ms (77.21%) | Memory: 11.2 MB (55.77%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,31 +1,4 @@
 class Solution {
-public:
-    int beautySum(string s) {
-        int n = s.size();
-        int ans = 0;
-
-        // window length from 3 to n
-        for (int len = 3; len <= n; ++len) {
-
-            vector<int> freq(26, 0);
-            // initialize first window
-            for (int i = 0; i < len; i++) {
-                freq[s[i] - 'a']++;
-            }
-
-            ans += beauty(freq);
-
-            // slide window
-            for (int i = len; i < n; i++) {
-                freq[s[i] - 'a']++;               // add new char
-                freq[s[i - len] - 'a']--;         // remove old char
-                ans += beauty(freq);
-            }
-        }
-        return ans;
-    }
-
-    // helper to compute max freq - min nonzero freq
     int beauty(const vector<int>& freq) {
         int mx = 0, mn = INT_MAX;
         for (int f : freq) {
@@ -35,5 +8,24 @@ public:
             }
         }
         return mx - mn;
+    }
+
+public:
+    int beautySum(string s) {
+        int n = s.size();
+        int ans = 0;
+        for (int len = 3; len <= n; ++len) {
+            vector<int> freq(26, 0);
+            for (int i = 0; i < len; i++) {
+                freq[s[i] - 'a']++;
+            }
+            ans += beauty(freq);
+            for (int i = len; i < n; i++) {
+                freq[s[i] - 'a']++;
+                freq[s[i - len] - 'a']--;
+                ans += beauty(freq);
+            }
+        }
+        return ans;
     }
 };
