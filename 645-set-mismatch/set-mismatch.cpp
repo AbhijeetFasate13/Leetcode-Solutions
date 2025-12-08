a9a8203174c30bf8197eb8a4d29990a7df0530dd
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-12-08 21:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (77.58%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,20 +1,23 @@
 class Solution {
+    void cyclicSort(vector<int>& nums, int n) {
+        for (int i = 0; i < n; i++) {
+            while (nums[i] != i + 1 and nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+    }
+
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        vector<int>hash(n+1);
-        for(auto i:nums){
-            hash[i]++;
-        }
-        int repeat,needed=n;
-        for(int i=1;i<=n;i++){
-            if(hash[i]==0){
-                needed=i;
-            }
-            if(hash[i]==2){
-                repeat=i;
+        cyclicSort(nums, n);
+        vector<int> errorNums(2);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                errorNums[0] = nums[i];
+                errorNums[1] = i + 1;
             }
         }
-        return {repeat,needed};
+        return errorNums;
     }
 };
