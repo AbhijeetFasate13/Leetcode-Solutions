88b5d074cf76eed39c3da8d2796fd1c9e8c1171b
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-12-24 19:44:25 +0530

    Time: 9 ms (54.36%) | Memory: 39 MB (38.18%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -3,63 +3,29 @@ public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int m = maze.size(), n = maze[0].size();
         int r = entrance[0], c = entrance[1];
-        queue<pair<int, int>> q;
-        vector<vector<int>> shortestPath(m, vector<int>(n, INT_MAX));
-        shortestPath[r][c] = 0;
+        queue<pair<int,int>> q;
+        vector<vector<int>> dist(m, vector<int>(n, -1));
+        vector<vector<int>> dir = {{-1,0},{1,0},{0,-1},{0,1}};
         q.push({r, c});
+        dist[r][c] = 0;
         while (!q.empty()) {
             auto [nR, nC] = q.front();
             q.pop();
-            // top
-            if (nR != 0 and maze[nR - 1][nC] != '+' and
-                shortestPath[nR - 1][nC] == INT_MAX) {
-                q.push({nR - 1, nC});
-                shortestPath[nR - 1][nC] = 1 + shortestPath[nR][nC];
-            }
-            // bottom
-            if (nR != m - 1 and maze[nR + 1][nC] != '+' and
-                shortestPath[nR + 1][nC] == INT_MAX) {
-                q.push({nR + 1, nC});
-                shortestPath[nR + 1][nC] = 1 + shortestPath[nR][nC];
-            }
-            // left
-            if (nC != 0 and maze[nR][nC - 1] != '+' and
-                shortestPath[nR][nC - 1] == INT_MAX) {
-                q.push({nR, nC - 1});
-                shortestPath[nR][nC - 1] = 1 + shortestPath[nR][nC];
-            }
-            // right
-            if (nC != n - 1 and maze[nR][nC + 1] != '+' and
-                shortestPath[nR][nC + 1] == INT_MAX) {
-                q.push({nR, nC + 1});
-                shortestPath[nR][nC + 1] = 1 + shortestPath[nR][nC];
+            for (auto &d : dir) {
+                int newR = nR + d[0];
+                int newC = nC + d[1];
+                if (newR < 0 || newR >= m || newC < 0 || newC >= n) continue;
+                if (maze[newR][newC] == '+') continue;
+                if (dist[newR][newC] != -1) continue;
+                dist[newR][newC] = dist[nR][nC] + 1;
+                if (newR == 0 || newR == m - 1 || newC == 0 || newC == n - 1) {
+                    return dist[newR][newC];
+                }
+
+                q.push({newR, newC});
             }
         }
-        int ans = INT_MAX;
-        // top row
-        for (int i = 0; i < n; i++) {
-            if (r == 0 and c == i)
-                continue;
-            ans = min(ans, shortestPath[0][i]);
-        }
-        // bottom row
-        for (int i = 0; i < n; i++) {
-            if (r == m - 1 and c == i)
-                continue;
-            ans = min(ans, shortestPath[m - 1][i]);
-        }
-        // leftmost col
-        for (int i = 0; i < m; i++) {
-            if (r == i and c == 0)
-                continue;
-            ans = min(ans, shortestPath[i][0]);
-        }
-        // rightmost col
-        for (int i = 0; i < m; i++) {
-            if (r == i and c == n - 1)
-                continue;
-            ans = min(ans, shortestPath[i][n - 1]);
-        }
-        return ans == INT_MAX ? -1 : ans;
+
+        return -1;
     }
 };
