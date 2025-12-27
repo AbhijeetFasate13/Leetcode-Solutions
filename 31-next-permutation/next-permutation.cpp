84cbd7493e9972cbe5362b1411e34545976174a1
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-12-27 15:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (31.74%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        /*
+        1 2 3 4
+        1 2 4 3
+        1 3 2 4
+        1 3 4 2
+        1 4 2 3
+        1 4 3 2
+        2 1 3 4
+        2 1 4 3
+        2 3 1 4
+        2 3 4 1
+        2 4 1 3
+        2 4 3 1
+        3 1 2 4
+        */
         int n = nums.size();
-        int l = n - 2;
-        while (l >= 0 && nums[l] >= nums[l + 1]) {
-            l--;
-        }
-
-        if (l >= 0) {
-            int r = n - 1;
-            while (nums[r] <= nums[l]) {
-                r--;
+        int ind = -1;
+        for (int i = n - 1; i >= 1; i--) {
+            if (nums[i] > nums[i - 1]) {
+                int min = nums[i], minIdx = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (nums[j] > nums[i - 1] and min >= nums[j]) {
+                        min = nums[j];
+                        minIdx = j;
+                    }
+                }
+                swap(nums[i - 1], nums[minIdx]);
+                ind = i;
+                break;
             }
-            swap(nums[l], nums[r]);
         }
-        reverse(nums.begin() + l + 1, nums.end());
+        if (ind == -1) {
+            reverse(begin(nums), end(nums));
+        } else {
+            reverse(nums.begin() + ind, nums.end());
+        }
     }
 };
