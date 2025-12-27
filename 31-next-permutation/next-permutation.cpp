9b7866752997f5a0facd08ca6a9593f9a272220b
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-12-27 15:31:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (66.40%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        /*
-        1 2 3 4
-        1 2 4 3
-        1 3 2 4
-        1 3 4 2
-        1 4 2 3
-        1 4 3 2
-        2 1 3 4
-        2 1 4 3
-        2 3 1 4
-        2 3 4 1
-        2 4 1 3
-        2 4 3 1
-        3 1 2 4
-        */
         int n = nums.size();
         int ind = -1;
         for (int i = n - 1; i >= 1; i--) {
+            //find first pair which are ordered (asc)
             if (nums[i] > nums[i - 1]) {
                 int min = nums[i], minIdx = i;
+                //find the min element just greater than less of ordered pair
                 for (int j = i + 1; j < n; j++) {
                     if (nums[j] > nums[i - 1] and min >= nums[j]) {
                         min = nums[j];
                         minIdx = j;
                     }
                 }
+                //swap that min with lesser of ordered pair
                 swap(nums[i - 1], nums[minIdx]);
                 ind = i;
                 break;
             }
         }
+        //reverse from greater of ordered pair to end
         if (ind == -1) {
             reverse(begin(nums), end(nums));
         } else {
