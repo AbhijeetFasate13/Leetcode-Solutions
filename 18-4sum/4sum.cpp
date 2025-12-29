783b85a3a3a0faeb0002b38e131cb4cdd338e4ab
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-12-29 19:33:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (81.92%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -7,8 +7,21 @@ public:
         for (int i = 0; i < n - 3; ++i) {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
+
+            if (nums[i] * 1ll + nums[i + 1] + nums[i + 2] + nums[i + 3] >
+                target)
+                break;
+            if (nums[i] * 1ll + nums[n - 3] + nums[n - 2] + nums[n - 1] <
+                target)
+                continue;
             for (int j = i + 1; j < n - 2; ++j) {
                 if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                if (nums[i] * 1ll + nums[j + 1] + nums[j + 2] + nums[j] >
+                    target)
+                    break;
+                if (nums[i] * 1ll + nums[n - 1] + nums[n - 2] + nums[j] <
+                    target)
                     continue;
                 int left = j + 1, right = n - 1;
                 while (left < right) {
