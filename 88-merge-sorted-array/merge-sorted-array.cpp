c9d954da4c9f4f8ed6c96f1aa4ec42225ebbbae2
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2025-12-31 11:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (38.50%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>temp;
-        int ptr1 = 0, ptr2 = 0;
-        while(ptr1 != m and ptr2 != n){
-            if(nums1[ptr1]<nums2[ptr2]){
-                temp.push_back(nums1[ptr1]);
-                ptr1++;
-            }
-            else{
-                temp.push_back(nums2[ptr2]);
-                ptr2++;
+        int zeroElPtr = m + n - 1;
+        int n1 = m - 1, n2 = n - 1;
+        while (n1 >= 0 and n2 >= 0) {
+            if (nums1[n1] >= nums2[n2]) {
+                nums1[zeroElPtr--] = nums1[n1--];
+            } else {
+                nums1[zeroElPtr--] = nums2[n2--];
             }
         }
-        while(ptr1!=m){
-            temp.push_back(nums1[ptr1]);
-                ptr1++;
+        while (n1 >= 0) {
+            nums1[zeroElPtr--] = nums1[n1--];
         }
-        while(ptr2!=n){
-             temp.push_back(nums2[ptr2]);
-                ptr2++;
+        while (n2 >= 0) {
+            nums1[zeroElPtr--] = nums2[n2--];
         }
-        nums1=temp;
     }
 };
