76111bc6dddd7f18c567b354b0a901cf5da3aeff
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-01-04 10:18:51 +0530

    Time: 23 ms (60.59%) | Memory: 16 MB (92.61%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumOfDivisorsOfNum(int n) {
+        int sum = 0;
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+                count++;
+                if (n / i != i) {
+                    sum += n / i;
+                    count++;
+                }
+            }
+        }
+        return count == 4 ? sum : 0;
+    }
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+        for (const int& i : nums) {
+            ans += sumOfDivisorsOfNum(i);
+        }
+        return ans;
+    }
+};
