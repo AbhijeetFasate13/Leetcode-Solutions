ebb8ed728cf7ae44644b489c1fd73da911787dfb
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-01-12 13:50:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (26.76%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -3,14 +3,10 @@ public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int n = points.size();
         vector<int> curr = points[0];
-        int timeTaken = 0, diagonalMove, axisMove;
+        int timeTaken = 0;
         for (int i = 1; i < n; i++) {
-            diagonalMove =
-                min(abs(points[i][0] - curr[0]), abs(points[i][1] - curr[1]));
-            axisMove = abs(
-                max(abs(points[i][0] - curr[0]), abs(points[i][1] - curr[1])) -
-                diagonalMove);
-            timeTaken += diagonalMove + axisMove;
+            timeTaken +=
+                max(abs(points[i][0] - curr[0]), abs(points[i][1] - curr[1]));
             curr = points[i];
         }
         return timeTaken;
