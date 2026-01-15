5664bb8b5ab36a464d8a97115cc1cf8447935ff9
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-01-15 10:15:19 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (18.26%) - LeetSync

--- 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
@@ -1,29 +1,22 @@
 class Solution {
+    int getStreak(vector<int>& arr) {
+        sort(begin(arr), end(arr));
+        int streak = 1, len = 1;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] == arr[i - 1] + 1) {
+                len++;
+            } else {
+                streak = max(len, streak);
+                len = 1;
+            }
+        }
+        streak = max(len, streak);
+        return streak;
+    }
+
 public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars,
                                vector<int>& vBars) {
-        sort(begin(hBars), end(hBars));
-        sort(begin(vBars), end(vBars));
-        int streakH = 1, streakV = 1, len = 1;
-        for (int i = 1; i < hBars.size(); i++) {
-            if (hBars[i] == hBars[i - 1] + 1) {
-                len++;
-            } else {
-                streakH = max(len, streakH);
-                len = 1;
-            }
-        }
-        streakH = max(len, streakH);
-        len = 1;
-        for (int i = 1; i < vBars.size(); i++) {
-            if (vBars[i] == vBars[i - 1] + 1) {
-                len++;
-            } else {
-                streakV = max(len, streakV);
-                len = 1;
-            }
-        }
-        streakV = max(len, streakV);
-        return pow((min(streakH, streakV) + 1), 2);
+        return pow(min(getStreak(hBars), getStreak(vBars)) + 1,2);
     }
 };
