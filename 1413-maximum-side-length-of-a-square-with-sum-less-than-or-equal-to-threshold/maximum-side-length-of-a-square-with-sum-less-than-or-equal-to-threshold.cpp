57e1ef5a26faf488e7672b8c23b8dcca3910c3c7
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-01-19 17:12:38 +0530

    Time: 11 ms (69.55%) | Memory: 33.1 MB (7.27%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -2,54 +2,64 @@ static auto init = []() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    return nullptr;
+    return 0;
 }();
+
 class Solution {
     vector<vector<int>> prefix;
     int rows, cols;
-    void pre(const vector<vector<int>>& mat) {
-        prefix.assign(rows, vector<int>(cols));
+
+    void buildPrefix(const vector<vector<int>>& mat) {
+        prefix.assign(rows, vector<int>(cols, 0));
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                prefix[i][j] = (i > 0 ? prefix[i - 1][j] : 0) +
+                prefix[i][j] = mat[i][j] + (i > 0 ? prefix[i - 1][j] : 0) +
                                (j > 0 ? prefix[i][j - 1] : 0) -
-                               ((i > 0 and j > 0) ? prefix[i - 1][j - 1] : 0) +
-                               mat[i][j];
+                               (i > 0 && j > 0 ? prefix[i - 1][j - 1] : 0);
             }
         }
     }
 
-    bool check(const int& row, const int& col, const int& sz,
-               const int& threshold) {
-        int sumOfSquare = prefix[row + sz - 1][col + sz - 1] -
-                          (col > 0 ? prefix[row + sz - 1][col - 1] : 0) -
-                          (row > 0 ? prefix[row - 1][col + sz - 1] : 0) +
-                          (row > 0 and col > 0 ? prefix[row - 1][col - 1] : 0);
-        return sumOfSquare <= threshold;
+    bool validSquare(int r, int c, int sz, int threshold) {
+        int sum = prefix[r + sz - 1][c + sz - 1] -
+                  (r > 0 ? prefix[r - 1][c + sz - 1] : 0) -
+                  (c > 0 ? prefix[r + sz - 1][c - 1] : 0) +
+                  (r > 0 && c > 0 ? prefix[r - 1][c - 1] : 0);
+
+        return sum <= threshold;
     }
 
 public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
-        rows = mat.size(), cols = mat[0].size();
-        int ans = 0;
-        pre(mat);
-        for (int sz = 1; sz <= min(rows, cols); sz++) {
-            bool isSideLengthUnderThreshold = false;
-            for (int i = 0; i <= rows - sz; i++) {
-                for (int j = 0; j <= cols - sz; j++) {
-                    if (check(i, j, sz, threshold)) {
-                        isSideLengthUnderThreshold = true;
+        if (mat.empty() || mat[0].empty())
+            return 0;
+
+        rows = mat.size();
+        cols = mat[0].size();
+
+        buildPrefix(mat);
+
+        int l = 0, r = min(rows, cols);
+
+        while (l < r) {
+            int m = l + (r - l + 1) / 2;
+            bool ok = false;
+
+            for (int i = 0; i + m <= rows && !ok; i++) {
+                for (int j = 0; j + m <= cols; j++) {
+                    if (validSquare(i, j, m, threshold)) {
+                        ok = true;
                         break;
                     }
                 }
-                if (isSideLengthUnderThreshold) {
-                    break;
-                }
             }
-            if (isSideLengthUnderThreshold) {
-                ans = sz;
-            }
+
+            if (ok)
+                l = m;
+            else
+                r = m-1;
         }
-        return ans;
+
+        return l;
     }
 };
