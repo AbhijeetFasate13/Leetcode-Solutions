5124449a1c8927a0938a9226ee34e434cc980f6e
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-01-26 07:03:41 +0530

    Time: 15 ms (76.77%) | Memory: 36.7 MB (64.03%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -2,9 +2,9 @@ class Solution {
     int getMinDiff(vector<int>& a) {
         int n = a.size();
         sort(begin(a), end(a));
-        int minDiff = abs(a[1] - a[0]);
+        int minDiff = a[1] - a[0];
         for (int i = 1; i < n - 1; i++) {
-            minDiff = min(minDiff, abs(a[i + 1] - a[i]));
+            minDiff = min(minDiff, a[i + 1] - a[i]);
         }
         return minDiff;
     }
@@ -15,9 +15,8 @@ public:
         int n = arr.size();
         vector<vector<int>> ans;
         for (int i = 0; i < n - 1; i++) {
-            if (abs(arr[i + 1] - arr[i]) == minDiff) {
-                ans.push_back(
-                    {min(arr[i], arr[i + 1]), max(arr[i], arr[i + 1])});
+            if (arr[i + 1] - arr[i] == minDiff) {
+                ans.push_back({arr[i], arr[i + 1]});
             }
         }
         return ans;
