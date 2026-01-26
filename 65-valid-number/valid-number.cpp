09462b099dd735089051801997dda22e35d0167c
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-01-26 20:40:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (9.64%) - LeetSync

--- 65-valid-number/valid-number.cpp
+++ 65-valid-number/valid-number.cpp
@@ -1,103 +1,90 @@
 class Solution {
-    bool isDigit(const char& c) { return c >= '0' and c <= '9'; }
-    bool isLowerCaseAlpha(const char& c) { return c >= 'a' and c <= 'z'; }
-    bool isUpperCaseAlpha(const char& c) { return c >= 'A' and c <= 'Z'; }
-    bool isExpo(const char& c) { return c == 'e' or c == 'E'; }
-    bool isSign(const char& c) { return c == '-' or c == '+'; }
+    bool isDigit(char c) { return c >= '0' && c <= '9'; }
+    bool isExpo(char c) { return c == 'e' || c == 'E'; }
+    bool isSign(char c) { return c == '+' || c == '-'; }
 
     bool isInteger(const string& s) {
         if (s.empty())
             return false;
-        int n = s.size();
-        bool digitEncountered = false;
-        for (int i = 0; i < n; i++) {
-            if (isSign(s[i]) and i != 0)
-                return false;
-            if (s[i] == '.')
-                return false;
-            if (isDigit(s[i]))
-                digitEncountered = true;
-        }
-        return digitEncountered;
-        ;
-    }
-    bool isDecimal(const string& s) {
-        int n = s.size();
-        if (n <= 1)
-            return false;
-        bool dotFound = false;
-        int start = isSign(s[0]) ? 1 : 0;
-        bool digitEncountered = false;
-        for (int i = start; i < n; i++) {
-            if (isDigit(s[i]))
-                digitEncountered = true;
-            if (isSign(s[i]) and i != 0)
-                return false;
 
-            if (s[i] == '.') {
+        bool digitFound = false;
+        for (int i = 0; i < s.size(); i++) {
+            if (isSign(s[i]) && i != 0)
+                return false;
+            if (!isDigit(s[i]) && !isSign(s[i]))
+                return false;
+            if (isDigit(s[i]))
+                digitFound = true;
+        }
+        return digitFound;
+    }
+
+    bool isDecimal(const string& s) {
+        if (s.size() <= 1)
+            return false;
+
+        int start = isSign(s[0]) ? 1 : 0;
+        bool digitFound = false, dotFound = false;
+
+        for (int i = start; i < s.size(); i++) {
+            if (isDigit(s[i])) {
+                digitFound = true;
+            } else if (s[i] == '.') {
                 if (dotFound)
                     return false;
 
                 if ((i == start || !isDigit(s[i - 1])) &&
-                    (i + 1 >= n || !isDigit(s[i + 1])))
+                    (i + 1 >= s.size() || !isDigit(s[i + 1])))
                     return false;
 
                 dotFound = true;
+            } else {
+                return false;
             }
         }
-        return digitEncountered;
+        return digitFound;
     }
+
     int getExpoIdx(const string& s) {
-        int n = s.size();
-        int expoIdx = -1;
-        int expoCount = 0;
-        int signCount = 0;
+        int expoIdx = -1, expoCount = 0, signCount = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.size(); i++) {
             if (isExpo(s[i])) {
-                expoCount++;
                 expoIdx = i;
+                expoCount++;
                 signCount = 0;
-                if (i == 0 || i == n - 1)
-                    return INT_MIN;
-                if (!isDigit(s[i - 1]) and s[i - 1] != '.')
-                    return INT_MIN;
 
+                if (i == 0 || i == s.size() - 1)
+                    return INT_MIN;
+                if (!isDigit(s[i - 1]) && s[i - 1] != '.')
+                    return INT_MIN;
                 if (!isDigit(s[i + 1]) && !isSign(s[i + 1]))
                     return INT_MIN;
 
-                if (isSign(s[i + 1])) {
-                    if (i + 2 >= n || !isDigit(s[i + 2]))
-                        return INT_MIN;
-                }
-            } else if (isSign(s[i])) {
-                signCount++;
-                if (signCount > 1)
+                if (isSign(s[i + 1]) &&
+                    (i + 2 >= s.size() || !isDigit(s[i + 2])))
                     return INT_MIN;
-            } else if (isLowerCaseAlpha(s[i]) || isUpperCaseAlpha(s[i])) {
+            } else if (isSign(s[i])) {
+                if (++signCount > 1)
+                    return INT_MIN;
+            } else if (!isDigit(s[i]) && s[i] != '.') {
                 return INT_MIN;
             }
         }
-
-        if (expoCount > 1)
-            return INT_MIN;
-
-        return expoIdx;
+        return expoCount > 1 ? INT_MIN : expoIdx;
     }
 
 public:
     bool isNumber(string s) {
-        int n = s.size();
         int expoIdx = getExpoIdx(s);
-        cout << expoIdx << " ";
         if (expoIdx == INT_MIN)
             return false;
+
         if (expoIdx == -1)
-            return isInteger(s) or isDecimal(s);
+            return isInteger(s) || isDecimal(s);
 
-        string numBeforeExpo = s.substr(0, expoIdx);
-
-        return (isInteger(numBeforeExpo) or isDecimal(numBeforeExpo)) and
+        return (isInteger(s.substr(0, expoIdx)) ||
+                isDecimal(s.substr(0, expoIdx))) &&
                isInteger(s.substr(expoIdx + 1));
     }
 };
