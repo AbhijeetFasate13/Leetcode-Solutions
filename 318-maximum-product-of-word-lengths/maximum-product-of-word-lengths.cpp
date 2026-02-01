8aa87e4d535bdf1713346ce0b6cc160a75c18a73
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-01 19:55:11 +0530

    Time: 10 ms (50.79%) | Memory: 25.9 MB (93.57%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
@@ -1,35 +1,15 @@
 class Solution {
-    map<string, vector<int>> mp;
-    void mapStringToCharFreq(const vector<string>& words) {
-        for (const string& word : words) {
-            vector<int> freq(26);
-            for (const char& c : word) {
-                freq[c - 'a']++;
-            }
-            mp[word] = freq;
-            freq.clear();
-        }
-    }
-    bool checkIfShareCommonLetters(const vector<int>& a, const vector<int>& b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != 0 and b[i] != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 public:
     int maxProduct(vector<string>& words) {
-        mapStringToCharFreq(words);
-
         int n = words.size();
+        vector<int> mask(n);
         int maxi = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j)
-                    continue;
-                if (checkIfShareCommonLetters(mp[words[i]], mp[words[j]])) {
+            for (const char& c : words[i]) {
+                mask[i] |= (1 << (c - 'a'));
+            }
+            for (int j = 0; j < i; j++) {
+                if ((mask[i] & mask[j]) == 0) {
                     maxi = max(maxi, (int)(words[i].size() * words[j].size()));
                 }
             }
