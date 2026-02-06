dd38b2d56332c2a55a185dfd35755e87cf728627
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-06 13:18:35 +0530

    Time: 112 ms (66.42%) | Memory: 139.1 MB (93.43%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.cpp
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.cpp
@@ -16,17 +16,23 @@ public:
         int delete1 = n / 2, delete2 = n / 2;
         deleteDuplicates(nums1, mp1, delete1);
         deleteDuplicates(nums2, mp2, delete2);
-        unordered_map<int, int> mp;
-        for (const auto& i : mp1)
-            mp[i.first] = i.second;
-        for (const auto& i : mp2)
-            mp[i.first] += i.second;
-        int duplicates = 0;
-        for (const auto& i : mp) {
-            duplicates += i.second - 1;
+        int duplicates = 0, unique = 0;
+        for (const auto& i : mp1) {
+            if (mp2.find(i.first) == mp2.end()) {
+                unique++;
+                duplicates += i.second - 1;
+            } else {
+                unique++;
+                duplicates += i.second + mp2[i.first] - 1;
+            }
         }
+        for (const auto& i : mp2)
+            if (mp1.find(i.first) == mp1.end()) {
+                unique++;
+                duplicates += i.second - 1;
+            }
         int deletes = delete1 + delete2 - duplicates;
         deletes = max(0, deletes);
-        return mp.size() - deletes;
+        return unique - deletes;
     }
 };
