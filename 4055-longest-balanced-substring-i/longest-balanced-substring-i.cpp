70a49e2fdda87cc4f6e8a386d059d8d676774866
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-12 10:37:12 +0530

    Time: 404 ms (7.11%) | Memory: 11.2 MB (97.56%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    bool check(const vector<int>& arr) {
+        int sameFreq = -1;
+        for (const int& i : arr) {
+            if (i == 0)
+                continue;
+            if (sameFreq == -1)
+                sameFreq = i;
+            else if (sameFreq != i)
+                return false;
+        }
+        cout << endl;
+        return true;
+    }
+
+public:
+    int longestBalanced(string s) {
+        int n = s.size();
+        int maxLen = 1;
+        vector<int> freq(26);
+        for (int i = 0; i < n - maxLen; i++) {
+            fill(freq.begin(), freq.end(), 0);
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+                if (check(freq)) {
+                    maxLen = max(maxLen, j - i + 1);
+                }
+            }
+        }
+        return maxLen;
+    }
+};
