70c743dc7d2240dbe60e326e1eb1d92ba32df567
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-17 14:35:36 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (58.04%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,7 +12,7 @@
  */
 class Solution {
     void bfs(queue<pair<TreeNode*, pair<int, int>>>& q,
-             map<int, map<int, multiset<int>>>& mp) {
+             map<int, multiset<pair<int, int>>>& mp) {
         int n;
         while (!q.empty()) {
             n = q.size();
@@ -21,7 +21,7 @@ class Solution {
                 int row = curr.second.second;
                 int col = curr.second.first;
                 q.pop();
-                mp[col][row].insert(curr.first->val);
+                mp[col].insert({row, curr.first->val});
                 if (curr.first->left)
                     q.push({curr.first->left, {col - 1, row + 1}});
                 if (curr.first->right)
@@ -30,14 +30,12 @@ class Solution {
         }
     }
 
-    void fillAns(const map<int, map<int, multiset<int>>>& mp,
+    void fillAns(const map<int, multiset<pair<int, int>>>& mp,
                  vector<vector<int>>& ans) {
-        for (const auto col : mp) {
+        for (const auto it : mp) {
             vector<int> curr;
-            for (const auto it : col.second) {
-                for (const auto val : it.second) {
-                    curr.push_back(val);
-                }
+            for (const auto val : it.second) {
+                curr.push_back(val.second);
             }
             ans.push_back(curr);
         }
@@ -47,7 +45,7 @@ public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         if (!root)
             return {};
-        map<int, map<int, multiset<int>>> mp;
+        map<int, multiset<pair<int, int>>> mp;
         queue<pair<TreeNode*, pair<int, int>>> q;
         q.push({root, {0, 0}});
         bfs(q, mp);
