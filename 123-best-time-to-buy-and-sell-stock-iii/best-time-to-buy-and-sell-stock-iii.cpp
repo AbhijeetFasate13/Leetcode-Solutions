73a3ce417d1a46ade8bd32ac63f95c7bd4ad2c4a
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-19 21:15:10 +0530

    Time: 79 ms (59.61%) | Memory: 79.6 MB (70.11%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
+
         vector<vector<int>> curr(3, vector<int>(2, 0));
-        vector < vector<int>> prev(3, vector<int>(2, 0));
+        vector<vector<int>> prev(3, vector<int>(2, 0));
 
         for (int i = n - 1; i >= 0; i--) {
             for (int transactions = 0; transactions < 2; transactions++) {
@@ -17,11 +18,10 @@ public:
                             max(-prices[i] + prev[transactions][1],
                                 prev[transactions][0]);
                     }
-                    prev = curr;
                 }
             }
+            prev = curr;
         }
-
-        return curr[0][0];
+        return prev[0][0];
     }
 };
