93b3817da5515b0945f228e7c54e8bb05881eecb
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-21 20:27:03 +0530

    Time: 195 ms (75.19%) | Memory: 10.5 MB (83.62%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     bool helper(int row, int col, int idx, const string& cmp,
-                vector<vector<char>>& given, vector<vector<bool>>& visited) {
+                vector<vector<char>>& given) {
         if (idx == cmp.size())
             return true;
         if (row < 0 || row == given.size() || col < 0 ||
-            col == given[0].size() || visited[row][col] ||
-            given[row][col] != cmp[idx])
+            col == given[0].size() || given[row][col] != cmp[idx])
             return false;
 
-        visited[row][col] = true;
-        if (helper(row + 1, col, idx + 1, cmp, given, visited) ||
-            helper(row - 1, col, idx + 1, cmp, given, visited) ||
-            helper(row, col + 1, idx + 1, cmp, given, visited) ||
-            helper(row, col - 1, idx + 1, cmp, given, visited)) {
+        char c = given[row][col];
+        given[row][col] = '#';
+        if (helper(row + 1, col, idx + 1, cmp, given) ||
+            helper(row - 1, col, idx + 1, cmp, given) ||
+            helper(row, col + 1, idx + 1, cmp, given) ||
+            helper(row, col - 1, idx + 1, cmp, given)) {
             return true;
         }
-        visited[row][col] = false;
+        given[row][col] = c;
 
         return false;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size(), m = board[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if (board[i][j] == word[0]) {
-                    if (helper(i, j, 0, word, board, visited)) {
+                    if (helper(i, j, 0, word, board)) {
                         return true;
                     }
                 }
