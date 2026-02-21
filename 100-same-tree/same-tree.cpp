ab7c17bd9dd90e1c224a6b3e9d85217723ce380c
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-21 16:27:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (96.00%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -6,18 +6,18 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if ((!p and q) or (p and !q))
-            return false;
-        if (!p and !q)
-            return true;
-        return p->val == q->val and isSameTree(p->left, q->left) and
-               isSameTree(p->right, q->right);
+        if(!p or !q){
+            return p == q;
+        }
+        
+        bool l = isSameTree(p->left,q->left);
+        bool r = isSameTree(p->right,q->right);
+        return p->val == q->val and l and r;
     }
 };
