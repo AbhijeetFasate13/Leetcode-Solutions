e5640115d737463993f6b6b7083a1db5e2fa7210
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-21 23:31:05 +0530

    Time: 7 ms (27.77%) | Memory: 33 MB (61.90%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -1,30 +1,9 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
     ListNode* findMid(ListNode* head, ListNode*& prev) {
+        prev = nullptr;
         ListNode* slow = head;
         ListNode* fast = head;
-        prev = nullptr;
+
         while (fast && fast->next) {
             prev = slow;
             slow = slow->next;
@@ -37,13 +16,18 @@ public:
     TreeNode* sortedListToBST(ListNode* head) {
         if (!head)
             return nullptr;
-        ListNode* prevToMid = nullptr;
-        ListNode* mid = findMid(head, prevToMid);
-        if (prevToMid)
-            prevToMid->next = nullptr;
+
+        if (!head->next)
+            return new TreeNode(head->val);
+
+        ListNode* prev = nullptr;
+        ListNode* mid = findMid(head, prev);
+        if (prev)
+            prev->next = nullptr;
         TreeNode* root = new TreeNode(mid->val);
-        root->left = mid == head ? nullptr : sortedListToBST(head);
+        root->left = sortedListToBST(head);
         root->right = sortedListToBST(mid->next);
+
         return root;
     }
 };
