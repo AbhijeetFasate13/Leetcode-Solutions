f7d6c4e66620f1c0c7df45b7fa010277700e6a7f
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-22 00:09:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.18%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int reverse(int x) {
-        if (x == INT_MIN)
-            return 0;
         int rev = 0, lastDigit;
-        bool isNeg = x < 0 ? true : false;
-        if (isNeg)
-            x *= -1;
         while (x) {
             lastDigit = x % 10;
             x /= 10;
@@ -15,6 +10,6 @@ public:
             }
             rev = 10 * rev + lastDigit;
         }
-        return isNeg ? -rev : rev;
+        return rev;
     }
 };
