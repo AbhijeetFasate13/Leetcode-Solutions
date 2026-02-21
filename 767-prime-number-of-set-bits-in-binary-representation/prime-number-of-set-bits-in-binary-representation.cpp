45dc3309b689fc3f72591b99580510fd5d73aa17
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-21 10:42:31 +0530

    Time: 3 ms (72.17%) | Memory: 8 MB (61.81%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -4,7 +4,7 @@ class Solution {
             return false;
         if (n == 2)
             return true;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0)
                 return false;
         }
