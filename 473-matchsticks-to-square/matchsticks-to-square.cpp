fa5811b749b5ca93f51f48c10578dd148da31c2a
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-24 03:40:41 +0530

    Time: 246 ms (25.66%) | Memory: 12.9 MB (31.63%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/matchsticks-to-square.cpp
@@ -1,29 +1,34 @@
 class Solution {
-    bool rec(int idx, const int& target, vector<int>& lens,
-             const vector<int>& matches) {
-        if (idx == matches.size()) {
+public:
+    int maxSide;
+    bool solve(vector<int>& matchsticks, int i, vector<int>& lens) {
+        if (i == matchsticks.size()) {
             return lens[0] == lens[1] and lens[1] == lens[2] and
                    lens[2] == lens[3];
         }
-        for (int i = 0; i < 4; i++) {
-            if (matches[idx] + lens[i] > target)
-                continue;
-            lens[i] += matches[idx];
-            if (rec(idx + 1, target, lens, matches))
-                return true;
-            lens[i] -= matches[idx];
+
+        for (int j = 0; j < 4; j++) {
+            if (lens[j] + matchsticks[i] <= maxSide) {
+                lens[j] += matchsticks[i];
+                if (solve(matchsticks, i + 1, lens))
+                    return true;
+                lens[j] -= matchsticks[i];
+            }
         }
         return false;
     }
 
-public:
     bool makesquare(vector<int>& matchsticks) {
-        sort(matchsticks.rbegin(), matchsticks.rend());
-        vector<int> lens(4);
-        int sum = accumulate(matchsticks.begin(), matchsticks.end(), 0);
-        if (sum % 4 != 0)
+        if (matchsticks.size() < 4)
             return false;
-        sum /= 4;
-        return rec(0, sum, lens, matchsticks);
+        int totSum = accumulate(matchsticks.begin(), matchsticks.end(), 0);
+        if (totSum % 4 != 0)
+            return false;
+        maxSide = totSum / 4;
+        sort(matchsticks.rbegin(), matchsticks.rend());
+        if (matchsticks[0] > maxSide)
+            return false;
+        vector<int> lens(4);
+        return solve(matchsticks, 0, lens);
     }
 };
