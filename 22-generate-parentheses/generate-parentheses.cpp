924b37ee19813c1abc7b2bfc69c746e27e32450a
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-23 14:21:48 +0530

    Time: 4 ms (34.06%) | Memory: 13 MB (89.50%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,20 +1,32 @@
 class Solution {
-    void rec(int open, int close, string curr, vector<string>& allSets) {
-        if (open == 0 and close == 0) {
-            allSets.push_back(curr);
+    void backtrack(const int& n, int& open, int& close, string& curr,
+                   vector<string>& ans) {
+        if (curr.size() == 2 * n) {
+            if (open == close) {
+                ans.push_back(curr);
+            }
             return;
         }
-        if (open > 0)
-            rec(open - 1, close, curr + "(", allSets);
-        if (close > open) {
-            rec(open, close - 1, curr + ")", allSets);
+        curr.push_back('(');
+        open++;
+        backtrack(n, open, close, curr, ans);
+        open--;
+        curr.pop_back();
+        if (close < open) {
+            curr.push_back(')');
+            close++;
+            backtrack(n, open, close, curr, ans);
+            close--;
+            curr.pop_back();
         }
     }
 
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string> allSets;
-        rec(n, n, "", allSets);
-        return allSets;
+        string curr;
+        vector<string> ans;
+        int open = 0, close = 0;
+        backtrack(n, open, close, curr, ans);
+        return ans;
     }
 };
