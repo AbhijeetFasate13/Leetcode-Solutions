97812cac15e1c1472b0e92da6b78496f2f00c8a1
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-23 14:02:36 +0530

    Time: 27 ms (94.91%) | Memory: 53.4 MB (92.91%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool isPalindrome(const string& word) {
+        int start = 0, end = word.length() - 1;
+        while (start < end) {
+            if (word[start++] != word[end--])
+                return false;
+        }
+        return true;
+    }
+
+    void backtrack(string& s, int start, vector<string>& curr,
+                   vector<vector<string>>& ans) {
+        if (start == s.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            string newWord = s.substr(start, end - start + 1);
+            if (isPalindrome(newWord)) {
+                curr.push_back(newWord);
+                backtrack(s, end + 1, curr, ans);
+                curr.pop_back();
+            }
+        }
+    }
+
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> curr;
+        backtrack(s, 0, curr, ans);
+        return ans;
+    }
+};
