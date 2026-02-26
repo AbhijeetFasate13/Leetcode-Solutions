5b30e71280d354478adb42eb202ef78a5c3c4b36
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-02-26 19:59:11 +0530

    Time: 1 ms (71.36%) | Memory: 93.8 MB (89.51%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.cpp
+++ 3189-find-champion-ii/find-champion-ii.cpp
@@ -1,23 +1,9 @@
 class Solution {
-    vector<vector<int>> buildAdjMat(const int& n,
-                                    const vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n, vector<int>(n));
-        for (const auto& edge : edges) {
-            adj[edge[0]][edge[1]] = 1;
-        }
-        return adj;
-    }
-
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj = buildAdjMat(n, edges);
         vector<int> loser(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (adj[i][j] == 1) {
-                    loser[j]++;
-                }
-            }
+        for (const auto& edge : edges) {
+            loser[edge[1]] = 1;
         }
         int winner = -1;
         for (int i = 0; i < n; i++) {
