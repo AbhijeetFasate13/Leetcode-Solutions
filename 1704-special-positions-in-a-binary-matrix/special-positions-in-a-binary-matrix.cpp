a619aaa03270477e735fb5464ecae6fa864b8fc3
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-03-04 09:32:13 +0530

    Time: 1 ms (28.09%) | Memory: 16.5 MB (85.69%) - LeetSync

---
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    bool checkRowCol(const int& i, const int& j, const int& m, const int& n,
+                     vector<vector<int>>& mat) {
+        for (int k = 0; k < n; k++) {
+            if (k == j)
+                continue;
+            if (mat[i][k] == 1)
+                return false;
+        }
+        for (int k = 0; k < m; k++) {
+            if (k == i)
+                continue;
+            if (mat[k][j] == 1)
+                return false;
+        }
+        return true;
+    }
+
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 and checkRowCol(i, j, m, n, mat)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
