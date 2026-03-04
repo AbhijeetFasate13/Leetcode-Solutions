8806f9f066f674db6aac13b866b561dfc458885f
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-03-04 10:39:08 +0530

    Time: 104 ms (45.59%) | Memory: 163 MB (65.52%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        
+
         vector<int> rowCount(m, 0), colCount(n, 0);
-        unordered_map<int, pair<int,int>> position;
+        unordered_map<int, int> position;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                position[mat[i][j]] = {i, j};
+                position[mat[i][j]] = i * n + j;
             }
         }
-
+        int idx, row, col;
         for (int i = 0; i < arr.size(); i++) {
-            auto [row, col] = position[arr[i]];
+            idx = position[arr[i]];
+            col = idx % n;
+            row = (idx - col) / n;
             rowCount[row]++;
             colCount[col]++;
             if (rowCount[row] == n || colCount[col] == m)
