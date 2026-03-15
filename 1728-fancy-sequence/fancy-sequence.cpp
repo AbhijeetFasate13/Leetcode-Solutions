69124c4021e7bc4e8aeba2e69ea4a8699939528c
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-03-15 12:10:20 +0530

    Time: 74 ms (62.04%) | Memory: 192.3 MB (85.40%) - LeetSync

---
+++ 1728-fancy-sequence/fancy-sequence.cpp
@@ -0,0 +1,40 @@
+class Fancy {
+public:
+    const long MOD = 1e9 + 7;
+
+    vector<long> seq;
+    long add = 0;
+    long mul = 1;
+
+    long modInverse(long x) {
+        long res = 1, p = MOD - 2;
+        while (p) {
+            if (p & 1)
+                res = (res * x) % MOD;
+            x = (x * x) % MOD;
+            p >>= 1;
+        }
+        return res;
+    }
+
+    Fancy() {}
+
+    void append(int val) {
+        long v = (val - add + MOD) % MOD;
+        v = (v * modInverse(mul)) % MOD;
+        seq.push_back(v);
+    }
+
+    void addAll(int inc) { add = (add + inc) % MOD; }
+
+    void multAll(int m) {
+        mul = (mul * m) % MOD;
+        add = (add * m) % MOD;
+    }
+
+    int getIndex(int idx) {
+        if (idx >= seq.size())
+            return -1;
+        return (seq[idx] * mul + add) % MOD;
+    }
+};
