31cf6448513ead249d9bf1c7a667edf643865969
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-04-10 21:57:29 +0530

    Time: 3 ms (71.85%) | Memory: 30.8 MB (15.76%) - LeetSync

---
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3)
+            return -1;
+        map<int, vector<int>> mp;
+        bool exists = false;
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+            if (mp[nums[i]].size() > 2)
+                exists = true;
+        }
+        if (!exists)
+            return -1;
+        int minm = INT_MAX, sz;
+        vector<int> curr;
+        for (const auto& i : mp) {
+            curr = i.second;
+            sz = curr.size();
+            if (sz < 3)
+                continue;
+            for (int j = 0; j < sz - 2; j++) {
+                minm = min(minm, abs(curr[j] - curr[j + 1]) +
+                                     abs(curr[j + 1] - curr[j + 2]) +
+                                     abs(curr[j] - curr[j + 2]));
+            }
+        }
+        return minm;
+    }
+};
