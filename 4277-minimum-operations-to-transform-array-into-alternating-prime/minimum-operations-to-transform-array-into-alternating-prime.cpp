95a93c671f9586d3abdd9ff49bf07a8b3bffc5bd
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-04-12 00:49:14 +0530

    Time: 807 ms (17.14%) | Memory: 302.5 MB (11.35%) - LeetSync

---
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    const int MAXM = 200005;
+    vector<int> isPrime, nextPrime;
+
+public:
+    Solution() {
+        isPrime.assign(MAXM, 1);
+        isPrime[0] = isPrime[1] = 0;
+        for (int i = 2; i * i < MAXM; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < MAXM; j += i) {
+                    isPrime[j] = 0;
+                }
+            }
+        }
+        nextPrime.resize(MAXM);
+        int last = -1;
+        for (int i = MAXM - 1; i >= 0; i--) {
+            if (isPrime[i])
+                last = i;
+            nextPrime[i] = last;
+        }
+    }
+
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int cost = 0, x;
+        for (int i = 0; i < n; i++) {
+            x = nums[i];
+            if (i % 2 == 0)
+                cost += nextPrime[x] - x;
+            else
+                cost += isPrime[x] ? (x == 2 ? 2 : 1) : 0;
+        }
+        return cost;
+    }
+};
