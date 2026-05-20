b410a1a8e1f23b12db664b364327055d52c450a8
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-05-20 15:55:57 +0530

    Time: 3 ms (73.50%) | Memory: 85.7 MB (85.47%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -6,7 +6,11 @@ public:
         vector<int> freq(n);
         int common = 0;
         for (int i = 0; i < n; i++) {
-
+            if (A[i] == B[i]) {
+                common++;
+                ans[i] = common;
+                continue;
+            }
             freq[A[i] - 1]++;
             if (freq[A[i] - 1] == 2)
                 common++;
