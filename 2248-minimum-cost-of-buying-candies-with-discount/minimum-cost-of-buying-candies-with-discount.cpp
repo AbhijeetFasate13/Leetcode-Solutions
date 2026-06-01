53c66591e994c2f139e0d9519062a8fbbaeece2a
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-06-01 11:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (10.30%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     int minimumCost(vector<int>& cost) {
         sort(cost.rbegin(), cost.rend());
-        int n = cost.size();
+
         int totalCost = 0;
-        for (int i = 0; i < n; i++) {
-            if ((i + 1) % 3 == 0)
-                continue;
+        int n = cost.size();
+
+        for (int i = 0; i < n; i += 3) {
             totalCost += cost[i];
+
+            if (i + 1 < n)
+                totalCost += cost[i + 1];
         }
+
         return totalCost;
     }
 };
