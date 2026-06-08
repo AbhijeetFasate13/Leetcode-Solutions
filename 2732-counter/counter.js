2f03d2f72a03eb6836c31ed38fb484c7e2e7b9aa
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-06-08 13:55:01 +0530

    Time: 36 ms (87.07%) | Memory: 52.2 MB (97.29%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -2,9 +2,9 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
-    
-    return function() {
+var createCounter = function (n) {
+
+    return function () {
         return n++;
     };
 };
