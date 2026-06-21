35cd7aa1f1aa2a968726197741b07324ad00c076
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-06-22 03:30:18 +0530

    Time: 95 ms (5.36%) | Memory: 102.4 MB (5.44%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,18 +1,26 @@
 class Solution {
-public:
-    int maxIceCream(vector<int>& costs, int coins) {
+    void countSort(vector<int>& costs) {
         unordered_map<int, int> hash;
-        for (auto& i : costs) {
+        int maxm = INT_MIN;
+        for (const int& i : costs) {
             hash[i]++;
+            maxm = max(maxm, i);
         }
         int ptr = 0;
-        for (int i = 0; i < 1e5 + 1; i++) {
+        for (int i = 0; i <= maxm; i++) {
+            if (!hash.count(i))
+                continue;
             while (hash[i]--) {
                 costs[ptr++] = i;
             }
         }
+    }
+
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        countSort(costs);
         int count = 0;
-        for (auto& i : costs) {
+        for (const int& i : costs) {
             if (coins - i >= 0) {
                 count++;
                 coins -= i;
