a48a4692a9db843f222878a4fee26b1664dfc387
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-08-31 11:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 124.4 MB (95.97%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,32 +11,32 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        if (!head->next and !head->next->next)
-            return {-1, -1};
         ListNode* prev = head;
         ListNode* curr = head->next;
         int idx = 1, prevCritIdx = -1, firstCritIdx = -1;
-        vector<int> ans(2, INT_MAX);
+        vector<int> ans(2, -1);
 
         while (curr->next) {
             if ((curr->val > prev->val and curr->val > curr->next->val) or
                 (curr->val < prev->val and curr->val < curr->next->val)) {
-                if (firstCritIdx == -1) {
+
+                if (firstCritIdx == -1)
                     firstCritIdx = idx;
-                }
-                ans[0] = prevCritIdx == -1 ? INT_MAX
-                                           : min(ans[0], idx - prevCritIdx);
-                ans[1] = idx - firstCritIdx;
+
+                if (prevCritIdx != -1)
+                    ans[0] = ans[0] == -1 ? idx - prevCritIdx
+                                          : min(ans[0], idx - prevCritIdx);
+
+                if (idx != firstCritIdx)
+                    ans[1] = idx - firstCritIdx;
                 prevCritIdx = idx;
             }
+
             prev = curr;
             curr = curr->next;
             idx++;
         }
-        if (ans[0] == INT_MAX)
-            ans[0] = -1;
-        if (ans[1] == INT_MAX or ans[1]==0)
-            ans[1] = -1;
+
         return ans;
     }
 };
