e30f9a939be144babb3157159793330192658ea7
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-09-08 23:14:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int ans = 0;
-        for (int i = 1000; i <= n; i++)
-            ans += (to_string(i).length() - 1) / 3;
-        return ans;
+        if (n < 1000)
+            return 0;
+        return n - 999;
     }
 };
