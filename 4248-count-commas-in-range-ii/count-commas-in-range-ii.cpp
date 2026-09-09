4d325f7cd3bcd44d62935b475653b1debb0f63a0
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-09-09 10:43:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (57.33%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -4,9 +4,12 @@ public:
         if (n < 1000)
             return 0;
 
-        int digits = to_string(n).length();
-        long long commas = (digits - 1) / 3;
-        long long base = pow(10, digits - 1);
-        return commas * (n - base + 1) + countCommas(base - 1);
+        long long commas = 0;
+        long long base = 1000;
+        while (n >= base) {
+            commas += n - base + 1;
+            base *= 1000;
+        }
+        return commas;
     }
 };
